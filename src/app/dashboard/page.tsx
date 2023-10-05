4a090c5f53feeba0a0284a44c32b599893d1d8d3
Ossy Lab <ossyisibor@gmail.com>
2023-10-05 22:50:21 +0100

    setup for deployment

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,6 @@ import Header from "@/components/shared/Header";
 import Main from "@/components/shared/Main";
 import MobileNavbar from "@/components/shared/MobileNavbar";
 import Navbar from "@/components/shared/Navbar";
-import Spinner from "@/components/ui/Spinner";
-// import { getFeedbacks } from "@/hooks/useFeedbacks";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -11,7 +9,7 @@ export const metadata: Metadata = {
   description: "Product feedback app",
 };
 
-const Dashboard = async () => {
+const Dashboard = () => {
   // const allFeedbacks = await getFeedbacks();
 
   // if (allFeedbacks === undefined) {
@@ -20,13 +18,13 @@ const Dashboard = async () => {
 
   return (
     <div className="min-h-screen lg:grid lg:grid-cols-7 lg:py-[9.4rem] bg-ghost-white-100  xl:px-[10.5rem] lg:px-[3rem] ">
-      <Header />
+      {/* <Header />
       <Navbar />
 
       <div className=" relative min-h-screen bg-ghost-white-100  md:px-[3.9rem] lg:col-start-3 lg:col-end-8 lg:pt-6 lg:px-1  ">
         <MobileNavbar />
         <Main />
-      </div>
+      </div> */}
     </div>
   );
 };
